ec28604b350eba0994cb7c530cb0b84828bcc148
Beki Junior <bbekijunior1@gmail.com>
2025-07-27 23:22:10 +0300

    add routes to allow some before login

--- components/DataContext.jsx
+++ components/DataContext.jsx
@@ -13,15 +13,18 @@ export function FetchedDataProvider({ children }) {
         const token = localStorage.getItem('token');
 
         if (!token) {
+            // paths allowed without token
+            const allowedPaths = ['/adminRegister', '/login']
+
             // this prevent/avoids multiple renders and re-navigation/infinite re-rendering
-            if (router.pathname !== '/login') {
-                router.replace('/login');
+            if (!token && !allowedPaths.includes(router.pathname)) {
+                router.push('/login')
             }
         } else {
             fetch('/api/service')
                 .then(res => res.json())
                 .then(json => {
-                    setFetchedData(json.data || []);
+                    setFetchedData(json.data || [])
                 })
                 .catch(err => {
                     console.error("Failed to fetch data", err);
