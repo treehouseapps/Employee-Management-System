ada1db20a281937c077fd70b2339c25d7128d754
Beki Junior <bbekijunior1@gmail.com>
2025-08-05 21:16:03 +0300

    update loading

--- components/loading.jsx
+++ components/loading.jsx
@@ -2,7 +2,9 @@ export default function MyPage() {
     return (
         <div>
             <l-mirage
-                
+                size="60"
+                speed="2.5"
+                color="white"
             ></l-mirage>
         </div>
     );
